0593d5696cbe821f4514ddcbd490d3a6604139fa
kristoferek <krzysztof.michal.nowak@gmail.com>
2017-10-06 16:51:34 +0200

    Implemented phrase complexity

--- project 5/project5/js/app.js
+++ project 5/project5/js/app.js
@@ -8,7 +8,7 @@ var refusals = [
   "I think",
   "In my humble opinion",
   "After a careful analisys I realize that",
-  "After a deep thought of I conclude that"
+  "After a deep thought I conclude that"
 ];
 var reasons = [
   "is not the most desired activity for me at the moment.",
@@ -25,8 +25,9 @@ function phrase(salutation, name, refusal, activity, reason, recapitulation){
 }
 // Prototype function displaying complete phrase in selector with passed id
 phrase.prototype.display = function(id) {
-      var result = document.getElementById(id);
-      result.innerText = this.value;
+  var result = document.getElementById(id);
+  result.classList.remove('sample');
+  result.innerText = this.value;
 }
 
 // Function returning inut values and taking care for empty input fields
@@ -49,19 +50,37 @@ function getRandomIndex(arrayLength) {
 
 // Function displaying phrase depending input
 function handleGeneratePhrase(){
-  var activity = handleInput('activity');
   var interlocutorName = handleInput('interlocutor');
+  var activity = handleInput('activity');
 
   // If interlocutor and activity are specified
   if (activity!=="" && interlocutorName!=="") {
+    // Make interlocutor name lowercase with first capital letter
+    interlocutorName = interlocutorName.substr(0, 1).toUpperCase() + interlocutorName.substr(1, interlocutorName.length-1).toLowerCase();
+    // Make activity lowercase
+    activity = activity.toLowerCase();
+    activityUpperCase =  activity.substr(0, 1).toUpperCase() + activity.substr(1, activity.length-1);
+
     // Generate random indexes
     var salutationIndex = getRandomIndex(salutations.length);
     var refusalIndex =  getRandomIndex(refusals.length);
     var reasonsIndex = getRandomIndex(reasons.length);
     var recapitulationIndex =  getRandomIndex(recapitulations.length);
 
-    // Generate new phrase
-    var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, refusals[refusalIndex], activity, reasons[reasonsIndex], recapitulations[recapitulationIndex]);
+    // Generate randomly composed new phrase depending on chosen complexity
+    var complexity = document.getElementById('complexity').value;
+    console.log(complexity);
+    switch (complexity) {
+      case '2':
+        var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, refusals[refusalIndex], activity, reasons[reasonsIndex],"");
+        break;
+      case '3':
+        var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, refusals[refusalIndex], activity, reasons[reasonsIndex], recapitulations[recapitulationIndex]);
+        break;
+      default:
+        var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, "", activityUpperCase, reasons[reasonsIndex], "");
+
+    }
     // Display new phrase
     newPhrase.display("answer");
   }
