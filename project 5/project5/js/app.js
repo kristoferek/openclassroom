0b835b7277672f5099f4b84a71670ad561dcfbe3
kristoferek <krzysztof.michal.nowak@gmail.com>
2017-10-11 21:59:25 +0200

    JavaScript range handle, styling and meta snippets

--- project 5/project5/js/app.js
+++ project 5/project5/js/app.js
@@ -20,7 +20,7 @@ var refusals = [
   "I really appreciate your proposal, but",
   "Normally I'd be able to, but",
   "I wish I could, but",
-  "I'd love to but"
+  "I'd love to, but"
 ];
 var reasons = [
   "is not the most desired activity for me at the moment.",
@@ -55,17 +55,18 @@ phrase.prototype.display = function(id) {
   result.innerText = this.value;
 }
 
-// Function returning inut values and taking care for empty input fields
+// Function validating and returning input values
 function handleInput(id){
   var inputField = document.querySelector(`input[id="${id}"]`);
 
-  // If input field is empty show next element
-  if (inputField.value === "") {
+  // If input field is empty or not "a word" show warning
+  if (inputField.value.search(/\w/) < 0) {
     inputField.nextElementSibling.classList.remove('hidden');
+    return undefined;
   } else {
     inputField.nextElementSibling.classList.add('hidden');
+    return inputField.value;
   }
-  return inputField.value;
 }
 
 // Generate random index function
@@ -73,17 +74,31 @@ function getRandomIndex(arrayLength) {
     return Math.floor(Math.random() * (arrayLength));
 }
 
+// Make string Title Case
+function titleCase(string){
+  var title= "";
+  var nextCharUpperCase = true;
+  for (var i = 0; i < string.length; i++) {
+    title += nextCharUpperCase
+      ? string[i].toUpperCase()
+      : string[i];
+     nextCharUpperCase = (string[i] ===" ");
+  }
+  return title;
+}
+
 // Function displaying phrase depending input
 function handleGeneratePhrase(){
   var interlocutorName = handleInput('interlocutor');
   var activity = handleInput('activity');
 
-  // If interlocutor and activity are specified
-  if (activity!=="" && interlocutorName!=="") {
-    // Make interlocutor name lowercase with first capital letter
-    interlocutorName = interlocutorName.substr(0, 1).toUpperCase() + interlocutorName.substr(1, interlocutorName.length-1).toLowerCase();
-    // Make activity lowercase
+  // If interlocutor and activity inputs are not empty
+  if (activity!=undefined && interlocutorName!=undefined) {
+    // Make every interlocutor name lowercase with first capital letter
+    var interlocutorName = titleCase(interlocutorName);
+    // Make activity words lowercase in the middle of sentence
     activity = activity.toLowerCase();
+    // Make activity words lowercase with first letter capital at the  beggining of santence
     activityUpperCase =  activity.substr(0, 1).toUpperCase() + activity.substr(1, activity.length-1);
 
     // Generate random indexes
@@ -95,17 +110,37 @@ function handleGeneratePhrase(){
     // Generate randomly composed new phrase depending on chosen complexity
     var complexity = document.getElementById('complexity').value;
     switch (complexity) {
+      // moderately complex
       case '2':
         var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, refusals[refusalIndex], activity, reasons[reasonsIndex],"");
         break;
+      // complex
       case '3':
         var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, refusals[refusalIndex], activity, reasons[reasonsIndex], recapitulations[recapitulationIndex]);
         break;
+      // simple
       default:
         var newPhrase = new phrase(salutations[salutationIndex], interlocutorName, "", activityUpperCase, reasons[reasonsIndex], "");
 
     }
     // Display new phrase
     newPhrase.display("answer");
+  }
+}
+
+function handleComplexity(){
+  var inputRange = document.getElementById('complexity').value;
+  var buttonGenerate = document.getElementById('generate');
+  switch (inputRange) {
+    case '2':
+      buttonGenerate.innerHTML = "Generate <strong>medium</strong> phrase";
+      break;
+    case '3':
+      buttonGenerate.innerHTML = "Generate <strong>long</strong> phrase";
+      break;
+    default:
+    case '2':
+      buttonGenerate.innerHTML = "Generate <strong>short</strong> phrase";
+      break;
   }
 }
