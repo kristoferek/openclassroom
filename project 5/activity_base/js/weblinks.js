e5e23b028f1da29a67902632fc0e9b783858f565
kristoferek <krzysztof.michal.nowak@gmail.com>
2017-10-16 14:45:05 +0200

    Initial source

---
+++ project 5/activity_base/js/weblinks.js
@@ -0,0 +1,55 @@
+// List of links to show. Each link has:
+// - a title
+// - a URL
+// - an author (the person who added it)
+
+var linkList = [
+    {
+        title: "Kottke",
+        url: "http://kottke.org",
+        author: "brett.suggs"
+    },
+    {
+        title: "National Geographic",
+        url: "http://www.nationalgeographic.com",
+        author: "jessica"
+    },
+    {
+        title: "American Museum of Natural History",
+        url: "http://www.amnh.org",
+        author: "aurora.nicole"
+    }
+];
+
+function createLinkElement(link) {
+    var linktitle = document.createElement("a");
+    linktitle.href = link.url;
+    linktitle.style.color = "#428bca";
+    linktitle.style.textDecoration = "none";
+    linktitle.style.marginRight = "5px";
+    linktitle.appendChild(document.createTextNode(link.title));
+
+    var linkUrl = document.createElement("span");
+    linkUrl.appendChild(document.createTextNode(link.url));
+
+    var titleLine = document.createElement("h4");
+    titleLine.style.margin = "0px";
+    titleLine.appendChild(linktitle);
+    titleLine.appendChild(linkUrl);
+
+    var detailsLine = document.createElement("span");
+    detailsLine.appendChild(document.createTextNode("Added by " + link.author));
+
+    var linkDiv = document.createElement("div");
+    linkDiv.classList.add("link");
+    linkDiv.appendChild(titleLine);
+    linkDiv.appendChild(detailsLine);
+
+    return linkDiv;
+}
+
+var content = document.getElementById("content");
+linkList.forEach(function (link) {
+    var linkElement = createLinkElement(link);
+    content.appendChild(linkElement);
+});
